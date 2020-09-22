8c20cb9bdddf173f54519f2296e1e8e379efe56d
Sarah Shatto <Sarahshatto@gmail.com>
2020-09-21 23:53:33 -0700

    multiBracketValidation added

--- challenges/multiBracketValidation/__tests/multi-bracket-validation.js
+++ challenges/multiBracketValidation/__tests/multi-bracket-validation.js
@@ -1,3 +1,16 @@
 'use strict';
 
-multiBracketValidation(str)
+multiBracketValidation = function(str) {
+  let bracketUses = 0;
+
+  for(let i = 0; i < str.length; i++) {
+    if(str[i] == '[') {
+      bracketUses++;
+    }
+    else if(str[i] == ']') {
+      bracketUses--;
+    }
+  }
+
+  return bracketUses == 0;
+};
