9eeaed1d9a3a1266562bce692509bb2e6506ebed
Sarah Shatto <Sarahshatto@gmail.com>
2020-11-05 01:36:37 -0800

    CC27

--- challenges/challenges-26/__tests__/__insertionSort.tests.js
+++ challenges/challenges-26/__tests__/__insertionSort.tests.js
@@ -2,7 +2,7 @@
 
 it("should sort an unsorted array", () => {
   let unsortedArr = [8,4,23,42,16,15];
-  insertionSort(testArr);
+  insertionSort(unsortedArr);
   expect(unsortedArr).toEqual([4, 8, 15, 16, 23, 42]);
 });
 
